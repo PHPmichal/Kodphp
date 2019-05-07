a0036e626a600dbbed5d2ed058416a01626f808c
PHPmichal <49584632+PHPmichal@users.noreply.github.com>
2019-05-07 13:43:01 +0200

    Update AuctionType.php

--- AuctionType.php
+++ AuctionType.php
@@ -17,16 +17,16 @@ class AuctionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("title", TextType::class, ['label' => "Tytuł"])
-            ->add("description", TextareaType::class, ["label" => "Opis"])
-            ->add("price", NumberType::class, ["label" => "Cena"])
-            ->add("startingPrice", NumberType::class, ["label" => "Cena wywoławcza"])
+            ->add("title", TextType::class, ['label' => "Title"])
+            ->add("description", TextareaType::class, ["label" => "Description"])
+            ->add("price", NumberType::class, ["label" => "Price"])
+            ->add("startingPrice", NumberType::class, ["label" => "Starting price"])
             ->add(
                 "expiresAt",
                 DateTimeType::class,
-                ["label" => "Data zakończenia", "data" => new \DateTime("+1 day + 10 minutes")]
+                ["label" => "End Date", "data" => new \DateTime("+1 day + 10 minutes")]
             )
-            ->add("submit", SubmitType::class, ["label" => "Zapisz"]);
+            ->add("submit", SubmitType::class, ["label" => "Save"]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

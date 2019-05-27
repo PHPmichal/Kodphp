1899cddf2e4c613c69a53871f1ed66e90ba77811
PHPmichal <49584632+PHPmichal@users.noreply.github.com>
2019-05-27 08:42:56 +0200

    Update app-routing.module.ts

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,3 +1,5 @@
+//Tu znajdują się wszystkie Routes do poszczególnych podstron
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Router, RouterModule, Routes} from '@angular/router';
@@ -7,8 +9,8 @@ import {StatystykiComponent} from './statystyki/statystyki.component';
 import {UstawieniaComponent} from './ustawienia/ustawienia.component';
 import {AddproduktComponent} from './dodaj/addprodukt/addprodukt.component';
 
-const routes: Routes = [
-  {path: 'home' , component: HomeComponent},
+const routes: Routes = [ // Tutaj mamy Routes , które umożliwiają nam wywołanie danej metody w klasie
+  {path: 'home' , component: HomeComponent}, 
   {path: 'dodaj' , component: DodajComponent,
     children: [
       {path: 'addprodukt' , component: AddproduktComponent},

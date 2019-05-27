d3f8ad8258da79ec72a81f0bfe6abcf76b21e9f9
PHPmichal <49584632+PHPmichal@users.noreply.github.com>
2019-05-27 13:14:35 +0200

    Update TestDateExtension.php

--- TestDateExtension.php
+++ TestDateExtension.php
@@ -6,11 +6,12 @@ use AppBundle\Twig\DateExtension;
 
 class TestDateExtension extends \PHPUnit_Framework_TestCase
 {
-    public function testGetStyle()
+    public function testGetStyle() // testujemy zwracany styl w zależności od daty
     {
         $dateExtension = new DateExtension();
 
         $this->assertEquals("panel-default", $dateExtension->auctionStyle(new \DateTime("+2 days")));
+                        //wartość oczekiwana, Wartość rzeczywista 
         $this->assertEquals("panel-danger", $dateExtension->auctionStyle(new \DateTime("+20 minutes")));
     }
 }
